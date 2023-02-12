1268b51fe8571dbe2bebf3aa1c24054c1940df25
Jeremiah Torralba <jeremiahtorralba@gmail.com>
2023-02-12 01:26:31 -0500

    Add project files.

---
+++ Auth-Example/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Auth_Example.Startup))]
+namespace Auth_Example
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
